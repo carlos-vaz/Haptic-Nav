9df77c1f88eb4f6f64fe639021664e7b08ef76d5
Carlos Daniel Vazquez-Gomez Bertelli <crlsvzqz5@gmail.com>
2019-08-25 09:13:24 -0400

    only include tiny api ONCE

--- Vision/Segmentation/c/tiny_deeplab_api.hpp
+++ Vision/Segmentation/c/tiny_deeplab_api.hpp
@@ -1,6 +1,7 @@
 //TODO: include stuff only once
 
 #ifndef TINY_DEEPLAB_API_HPP_
+#define TINY_DEEPLAB_API_HPP_
 
 #include <tensorflow/c/c_api.h>
 
@@ -8,17 +9,15 @@ TF_Buffer* read_file(const char* file);
 void free_buffer(void* data, size_t length);
 void free_tensor(void* data, size_t length, void* args);
 
-struct segmap {
+typedef struct segmap {
 	const int64_t dims[2];
 	
-};
+} segmap_t;
 
-struct image {
+typedef struct image {
 	const int64_t dims[3];
-};
+} image_t;
 
-typedef struct segmap segmap_t;
-typedef struct image image_t;
 
 class Deeplab {
    private:
